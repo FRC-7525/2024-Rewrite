cad81e460ae456427151a7fa6446708d3a8a334e
PotmanNob <113220846+PotmanNob@users.noreply.github.com>
2024-07-11 19:30:45 -0500

    added triggers and removed flywheel (#2)
    added nisala triggers and removed the flywheel folder and stuff that related to it.

--- src/main/java/frc/robot/subsystems/Trigger.java
+++ src/main/java/frc/robot/subsystems/Trigger.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems;
 
-public class Trigger {
-    
+
+import java.util.function.BooleanSupplier;
+
+public class Trigger<StateType extends SubsystemStates> {
+    BooleanSupplier supplier;
+    StateType state;
+
+    public Trigger(BooleanSupplier supplier, StateType state) {
+        this.supplier = supplier;
+        this.state = state;
+    }
+
+    public boolean isTriggered() {
+        return supplier.getAsBoolean();
+    }
+
+    public StateType getResultState() {
+        return state;
+    }
 }
