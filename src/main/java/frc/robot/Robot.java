253a657fbee8cd344954edd1c0462b50a0bf4c06
Otto <grentomato@gmail.com>
2024-07-24 15:45:11 -0800

    actually uses stop function

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -106,7 +106,9 @@ public class Robot extends LoggedRobot {
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    managerSubsystem.stop();
+  }
 
   /** This function is called periodically when disabled. */
   @Override
