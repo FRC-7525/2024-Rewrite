eb3519cc002376cf797e1f6d3b1455d4cc1d0e2c
Liquid2112 <Shubhpatel2504@gmail.com>
2024-08-04 13:00:04 -0500

    Added Vision IO Real, Double checked constants, and got lines to tag poses

---
+++ src/main/java/frc/robot/subsystems/vision/VisionIOReal.java
@@ -0,0 +1,96 @@
+package frc.robot.subsystems.vision;
+
+import static java.lang.System.arraycopy;
+import static org.photonvision.PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import frc.robot.Constants;
+import java.util.Optional;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+
+public class VisionIOReal implements VisionIO {
+  // Forward Camera
+  private final PhotonCamera frontCam;
+  private final PhotonPoseEstimator frontPhotonPoseEstimator;
+
+  // Left Side Camera
+  private final PhotonCamera sideCam;
+  private final PhotonPoseEstimator sidePhotonPoseEstimator;
+
+  private Pose3d[] poseArray = new Pose3d[3];
+  private double[] timestampArray = new double[3];
+  private double[] visionStdArray = new double[9];
+  private double[] latencyArray = new double[3];
+  private int count = 0;
+
+  public VisionIOReal() {
+    frontCam = new PhotonCamera("front");
+    frontPhotonPoseEstimator =
+        new PhotonPoseEstimator(
+            Constants.Vision.aprilTagFieldLayout,
+            MULTI_TAG_PNP_ON_COPROCESSOR,
+            frontCam,
+            Constants.Vision.frontCamToRobot);
+
+    //  Side Camera
+    sideCam = new PhotonCamera("side");
+    sidePhotonPoseEstimator =
+        new PhotonPoseEstimator(
+            Constants.Vision.aprilTagFieldLayout,
+            MULTI_TAG_PNP_ON_COPROCESSOR,
+            sideCam,
+            Constants.Vision.sideCamToRobot);
+  }
+
+  @Override
+  public void updateInputs(VisionIOInputs inputs) {
+    getEstimatedPoseUpdates();
+    inputs.visionPoses = poseArray;
+    inputs.timestamps = timestampArray;
+    inputs.visionStdDevs = visionStdArray;
+    inputs.latency = latencyArray;
+    count += 1;
+    if (count % 500 == 0) {
+      frontCam.takeOutputSnapshot();
+      sideCam.takeOutputSnapshot();
+    }
+  }
+
+  public void getEstimatedPoseUpdates() {
+    Optional<EstimatedRobotPose> pose = frontPhotonPoseEstimator.update();
+    pose.ifPresentOrElse(
+        estimatedRobotPose -> {
+          poseArray[0] = estimatedRobotPose.estimatedPose;
+          timestampArray[0] = estimatedRobotPose.timestampSeconds;
+          Matrix<N3, N1> stdDevs =
+              getEstimationStdDevs(estimatedRobotPose, Constants.Vision.CameraResolution.HIGH_RES);
+          arraycopy(stdDevs.getData(), 0, visionStdArray, 0, 3);
+          latencyArray[0] = frontCam.getLatestResult().getLatencyMillis() / 1.0e3;
+        },
+        () -> {
+          poseArray[0] = new Pose3d();
+          timestampArray[0] = 0.0;
+          latencyArray[0] = 0.0;
+        });
+    pose = sidePhotonPoseEstimator.update();
+    pose.ifPresentOrElse(
+        estimatedRobotPose -> {
+          poseArray[1] = estimatedRobotPose.estimatedPose;
+          timestampArray[1] = estimatedRobotPose.timestampSeconds;
+          Matrix<N3, N1> stdDevs =
+              getEstimationStdDevs(estimatedRobotPose, Constants.Vision.CameraResolution.HIGH_RES);
+          arraycopy(stdDevs.getData(), 0, visionStdArray, 3, 3);
+          latencyArray[1] = sideCam.getLatestResult().getLatencyMillis() / 1.0e3;
+        },
+        () -> {
+          poseArray[1] = new Pose3d();
+          timestampArray[1] = 0.0;
+          latencyArray[1] = 0.0;
+        });
+  }
+}
--- src/main/java/frc/robot/subsystems/vision/VisionIOSim.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOSim.java
@@ -7,7 +7,6 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.proto.Trajectory;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Constants;
 import java.util.Optional;
@@ -19,132 +18,134 @@ import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;
 
 public class VisionIOSim implements VisionIO {
-    private final VisionSystemSim visionSim;
+  private final VisionSystemSim visionSim;
 
-    // Forward Camera
-    private final PhotonCameraSim frontCam;
-    private final PhotonPoseEstimator frontPhotonPoseEstimator;
+  // Forward Camera
+  private final PhotonCameraSim frontCam;
+  private final PhotonPoseEstimator frontPhotonPoseEstimator;
 
-    // Side Camera
-    private final PhotonCameraSim sideCam;
-    private final PhotonPoseEstimator sidePhotonPoseEstimator;
+  // Side Camera
+  private final PhotonCameraSim sideCam;
+  private final PhotonPoseEstimator sidePhotonPoseEstimator;
 
-    // Storage for Each Current Robot Pose (side and front cam)
-    private Pose3d[] poseArray = new Pose3d[2];
-    // Storage for vision measurment timestamp (last vision measurment)
-    private double[] timestampArray = new double[2];
-    // Storage for stds
-    private double[] visionStdArray = new double[6];
+  // Storage for Each Current Robot Pose (side and front cam)
+  private Pose3d[] poseArray = new Pose3d[2];
+  // Storage for vision measurment timestamp (last vision measurment)
+  private double[] timestampArray = new double[2];
+  // Storage for stds
+  private double[] visionStdArray = new double[6];
 
-    private boolean hasFrontVision = false;
-    private boolean hasSideVision = false;
+  private boolean hasFrontVision = false;
+  private boolean hasSideVision = false;
 
-    // Hahahha you cant stop me from making more timers, I win
-    private Timer sideVisionTimer = new Timer();
-    private Timer frontVisionTimer = new Timer();
+  // Hahahha you cant stop me from making more timers, I win
+  private Timer sideVisionTimer = new Timer();
+  private Timer frontVisionTimer = new Timer();
 
-    public VisionIOSim() {
-        PhotonCamera front = new PhotonCamera("front");
-        PhotonCamera side = new PhotonCamera("side");
-        frontVisionTimer.start();
-        sideVisionTimer.start();
+  public VisionIOSim() {
+    PhotonCamera front = new PhotonCamera("front");
+    PhotonCamera side = new PhotonCamera("side");
+    frontVisionTimer.start();
+    sideVisionTimer.start();
 
-        // No tags excluded, no point bc this is sim...
-        frontPhotonPoseEstimator = new PhotonPoseEstimator(
-                Constants.Vision.aprilTagFieldLayout,
-                MULTI_TAG_PNP_ON_COPROCESSOR,
-                front,
-                Constants.Vision.frontCamToRobot);
-        sidePhotonPoseEstimator = new PhotonPoseEstimator(
-                Constants.Vision.aprilTagFieldLayout,
-                MULTI_TAG_PNP_ON_COPROCESSOR,
-                side,
-                Constants.Vision.sideCamToRobot);
+    // No tags excluded, no point bc this is sim...
+    frontPhotonPoseEstimator =
+        new PhotonPoseEstimator(
+            Constants.Vision.aprilTagFieldLayout,
+            MULTI_TAG_PNP_ON_COPROCESSOR,
+            front,
+            Constants.Vision.frontCamToRobot);
+    sidePhotonPoseEstimator =
+        new PhotonPoseEstimator(
+            Constants.Vision.aprilTagFieldLayout,
+            MULTI_TAG_PNP_ON_COPROCESSOR,
+            side,
+            Constants.Vision.sideCamToRobot);
 
-        visionSim = new VisionSystemSim("main");
-        visionSim.addAprilTags(Constants.Vision.aprilTagFieldLayout);
+    visionSim = new VisionSystemSim("main");
+    visionSim.addAprilTags(Constants.Vision.aprilTagFieldLayout);
 
-        // TODO: Get accurate numbers & put these in constants
-        //Both Arducam OV9281, IDK IF THATS RIGHT
-        SimCameraProperties frontCameraProp = new SimCameraProperties();
-        frontCameraProp.setCalibration(1280, 800, Rotation2d.fromDegrees(84.47));
-        frontCameraProp.setCalibError(0.25, 0.10);
-        frontCameraProp.setFPS(40);
-        frontCameraProp.setAvgLatencyMs(40);
-        frontCameraProp.setLatencyStdDevMs(10);
+    // TODO: Get accurate numbers & put these in constants
+    // Both Arducam OV9281, IDK IF THATS RIGHT
+    SimCameraProperties frontCameraProp = new SimCameraProperties();
+    frontCameraProp.setCalibration(1280, 800, Rotation2d.fromDegrees(84.47));
+    frontCameraProp.setCalibError(0.25, 0.1);
+    frontCameraProp.setFPS(40);
+    frontCameraProp.setAvgLatencyMs(40);
+    frontCameraProp.setLatencyStdDevMs(10);
 
-        SimCameraProperties sideCameraProp = new SimCameraProperties(); // Arducam OV9281, not Spinel
-        sideCameraProp.setCalibration(1280, 800, Rotation2d.fromDegrees(84.47));
-        sideCameraProp.setCalibError(0.25, 0.10);
-        sideCameraProp.setFPS(40);
-        sideCameraProp.setAvgLatencyMs(40);
-        sideCameraProp.setLatencyStdDevMs(10);
+    SimCameraProperties sideCameraProp = new SimCameraProperties(); // Arducam OV9281, not Spinel
+    sideCameraProp.setCalibration(1280, 800, Rotation2d.fromDegrees(84.47));
+    sideCameraProp.setCalibError(0.25, 0.1);
+    sideCameraProp.setFPS(40);
+    sideCameraProp.setAvgLatencyMs(40);
+    sideCameraProp.setLatencyStdDevMs(10);
 
-        frontCam = new PhotonCameraSim(front, frontCameraProp);
-        sideCam = new PhotonCameraSim(side, sideCameraProp);
+    frontCam = new PhotonCameraSim(front, frontCameraProp);
+    sideCam = new PhotonCameraSim(side, sideCameraProp);
 
-        visionSim.addCamera(frontCam, Constants.Vision.frontCamToRobot);
-        visionSim.addCamera(sideCam, Constants.Vision.sideCamToRobot);
+    visionSim.addCamera(frontCam, Constants.Vision.frontCamToRobot);
+    visionSim.addCamera(sideCam, Constants.Vision.sideCamToRobot);
 
-        // Idk what this does
-        frontCam.enableDrawWireframe(true);
-        sideCam.enableDrawWireframe(true);
+    // Idk what this does
+    frontCam.enableDrawWireframe(true);
+    sideCam.enableDrawWireframe(true);
 
-        // Set things to stuff because becauseb before it would just spam null pointer
-        for (int i = 0; i < poseArray.length; i++) {
-            poseArray[i] = new Pose3d();
-        }
+    // Set things to stuff because becauseb before it would just spam null pointer
+    for (int i = 0; i < poseArray.length; i++) {
+      poseArray[i] = new Pose3d();
     }
+  }
 
-    @Override
-    public void updateInputs(VisionIOInputs inputs) {
-        getEstimatedPoseUpdates();
-        inputs.visionPoses = poseArray;
-        inputs.timestamps = timestampArray;
-        inputs.visionStdDevs = visionStdArray;
-        inputs.hasFrontVision = hasFrontVision;
-        inputs.hasSideVision = hasSideVision;
-    }
+  @Override
+  public void updateInputs(VisionIOInputs inputs) {
+    getEstimatedPoseUpdates();
+    inputs.visionPoses = poseArray;
+    inputs.timestamps = timestampArray;
+    inputs.visionStdDevs = visionStdArray;
+    inputs.hasFrontVision = hasFrontVision;
+    inputs.hasSideVision = hasSideVision;
+  }
 
-    public void updatePose(Pose2d pose) {
-        visionSim.update(pose);
-    }
+  public void updatePose(Pose2d pose) {
+    visionSim.update(pose);
+  }
 
-    public void getEstimatedPoseUpdates() {
-        Optional<EstimatedRobotPose> pose = frontPhotonPoseEstimator.update();
-        pose.ifPresentOrElse(
-                estimatedRobotPose -> {
-                    hasFrontVision = true;
-                    poseArray[0] = estimatedRobotPose.estimatedPose;
-                    timestampArray[0] = estimatedRobotPose.timestampSeconds;
-                    Matrix<N3, N1> stdDevs = getEstimationStdDevs(estimatedRobotPose,
-                            Constants.Vision.CameraResolution.HIGH_RES);
-                    arraycopy(stdDevs.getData(), 0, visionStdArray, 0, 3);
-                    frontVisionTimer.reset();
-                },
-                () -> {
-                    // If you just stop and input data doesent change this will eventually stop LOL
-                    if (frontVisionTimer.get() > Constants.Vision.LOST_VISION_THRESHOLD) {
-                        hasFrontVision = false;
-                    }
-                });
+  public void getEstimatedPoseUpdates() {
+    Optional<EstimatedRobotPose> pose = frontPhotonPoseEstimator.update();
+    pose.ifPresentOrElse(
+        estimatedRobotPose -> {
+          hasFrontVision = true;
+          poseArray[0] = estimatedRobotPose.estimatedPose;
+          timestampArray[0] = estimatedRobotPose.timestampSeconds;
+          Matrix<N3, N1> stdDevs =
+              getEstimationStdDevs(estimatedRobotPose, Constants.Vision.CameraResolution.HIGH_RES);
+          arraycopy(stdDevs.getData(), 0, visionStdArray, 0, 3);
+          frontVisionTimer.reset();
+        },
+        () -> {
+          // If you just stop and input data doesent change this will eventually stop LOL
+          if (frontVisionTimer.get() > Constants.Vision.LOST_VISION_THRESHOLD) {
+            hasFrontVision = false;
+          }
+        });
 
-        pose = sidePhotonPoseEstimator.update();
-        pose.ifPresentOrElse(
-                estimatedRobotPose -> {
-                    hasSideVision = true;
-                    poseArray[1] = estimatedRobotPose.estimatedPose;
-                    timestampArray[1] = estimatedRobotPose.timestampSeconds;
-                    Matrix<N3, N1> stdDevs = getEstimationStdDevs(estimatedRobotPose,
-                            Constants.Vision.CameraResolution.NORMAL);
-                    arraycopy(stdDevs.getData(), 0, visionStdArray, 3, 3);
-                    sideVisionTimer.restart();
-                },
-                () -> {
-                    // If you just stop and input data doesent change this will eventually stop LOL
-                    if (sideVisionTimer.get() > Constants.Vision.LOST_VISION_THRESHOLD) {
-                        hasSideVision = false;
-                    }
-                });
-    }
+    pose = sidePhotonPoseEstimator.update();
+    pose.ifPresentOrElse(
+        estimatedRobotPose -> {
+          hasSideVision = true;
+          poseArray[1] = estimatedRobotPose.estimatedPose;
+          timestampArray[1] = estimatedRobotPose.timestampSeconds;
+          Matrix<N3, N1> stdDevs =
+              getEstimationStdDevs(estimatedRobotPose, Constants.Vision.CameraResolution.NORMAL);
+          arraycopy(stdDevs.getData(), 0, visionStdArray, 3, 3);
+          sideVisionTimer.restart();
+        },
+        () -> {
+          // If you just stop and input data doesent change this will eventually stop LOL
+          if (sideVisionTimer.get() > Constants.Vision.LOST_VISION_THRESHOLD) {
+            hasSideVision = false;
+          }
+        });
+  }
 }
